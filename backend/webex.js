df57b2baa887f718d727b0ccece030d61e14ab19
Waldemar Kindler <waldemar.kindler@gmail.com>
2018-11-03 20:19:08 +0100

    Add member to newly created room

--- backend/webex.js
+++ backend/webex.js
@@ -4,6 +4,18 @@ exports.createMeeting = (title) => {
     spark.rooms.create({
         title: title
     })
+        .then(function(res) {
+            console.log(res)
+            spark.membership.create({
+                roomId: res.id,
+                personEmail: "codecampwaldemar@outlook.de",
+                isModerator: false
+            })
+                .catch(function(reason) {
+                    console.error(reason);
+                    process.exit(1);
+                });
+        })
     // Make sure to log errors in case something goes wrong.
         .catch(function(reason) {
             console.error(reason);
